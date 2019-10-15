3a708f71b9c6e814c98315a40cd58676f366da88
mnidokin <52515542+mnidokin@users.noreply.github.com>
2019-10-15 21:49:48 +0300

    Update main.c
    добавим-ка комментариев

--- fillit/src/main.c
+++ fillit/src/main.c
@@ -9,22 +9,8 @@ int main(int ac, char **av)
 
     if (ac !=  2)
         return (0);
-    tetra_nb = (ft_strlen((str = ft_reader_str(av[1]))) + 1) / 21;
-    coord_tab = ft_reader_coord_tab(str, tetra_nb, &min_map);
-    for (int p = 0; p < tetra_nb; p++)
-	{
-		for (int j = 0; j < 4; j++)
-			printf("%d ", coord_tab[p][j]);
-		printf("\n");
-	}
-	printf("\n\n");
-    coord_tab = ft_aux_coord_convert(min_map, tetra_nb, coord_tab);
-    for (int p = 0; p < tetra_nb; p++)
-	{
-		for (int j = 0; j < 4; j++)
-			printf("%d ", coord_tab[p][j]);
-		printf("\n");
-	}
-	printf("\n\n");
+    tetra_nb = (ft_strlen((str = ft_reader_str(av[1]))) + 1) / 21; // читаем файл в однну строку str и считаю количество элементов tetra_nb
+    coord_tab = ft_reader_coord_tab(str, tetra_nb, &min_map); // записываем координаты элементов, смещенных в левый верхний угол относительно карты 4х4, считаем минимальный размер карты
+    coord_tab = ft_aux_coord_convert(min_map, tetra_nb, coord_tab); // конвертируем координаты для карты размером min_map
     return (0);
 }
